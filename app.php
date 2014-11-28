4dfd0784d39a6959266f8ef4c7d2cec459e2338e
Keith <keith@vool.ie>
2014-11-28 20:58:38 +0000

    initial commitage

---
+++ app.php
@@ -0,0 +1,31 @@
+<?php
+
+// require config
+require_once('config.php');
+
+// require twitter class 
+require_once 'vendor/dg/twitter-php/src/twitter.class.php';
+
+// new twitter
+$twitter = new Twitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+	// grab the cam image 
+	$ch = curl_init(WEBCAM_URL);
+	$fp = fopen(TMP_IMG, 'wb');
+	curl_setopt($ch, CURLOPT_FILE, $fp);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_exec($ch);
+	curl_close($ch);
+	fclose($fp);
+
+try {
+	//tweet tweet
+	$tweet = $twitter -> send('', TMP_IMG);
+	
+	// remove the cam image 
+	unlink(TMP_IMG);
+	
+} catch (TwitterException $e) {
+	echo 'Error: ' . $e -> getMessage();
+}
+
---
+++ config-sample.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+* Main config file 
+* 
+* Rename this to config.php
+*   
+*/
+
+//Twitter configs 
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', '');
+define('ACCESS_TOKEN', '');
+define('ACCESS_TOKEN_SECRET', '');
+
+//Webcam configs
+
+define('WEBCAM_URL', 'http://atomik.kerrycoco.ie/webcams/webcam_ashe.asp');
+define('TMP_IMG', 'tmp/img.jpg');
+
