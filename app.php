bd31c142f63926df49298250cddf1c46bd2bea4f
Keith <keith@vool.ie>
2015-05-08 03:51:12 +0100

    Allow multiple cams, move config to json fie

--- app.php
+++ app.php
@@ -1,31 +1,44 @@
 <?php
 
 // require config
-require_once('config.php');
+$config = json_decode(file_get_contents('config.json'), true);
 
 // require twitter class 
 require_once 'vendor/dg/twitter-php/src/twitter.class.php';
 
-// new twitter
-$twitter = new Twitter(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+// settings
+$tmp_img = $config['tmp_img'];
 
-	// grab the cam image 
-	$ch = curl_init(WEBCAM_URL);
-	$fp = fopen(TMP_IMG, 'wb');
+
+foreach ($config['account'] as $t) {
+
+	// new twitter
+	$twitter = new Twitter($t['keys']['consumer_key'], $t['keys']['consumer_secret'], $t['keys']['access_token'], $t['keys']['access_token_secret']);
+
+	// grab the cam image
+	$ch = curl_init($t['webcam_url']);
+	$fp = fopen($tmp_img, 'wb');
 	curl_setopt($ch, CURLOPT_FILE, $fp);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_exec($ch);
 	curl_close($ch);
 	fclose($fp);
+	
+	// hashtags
+	$tags = implode( ' ', $t['hashtags'] );
+	
+	$tweet_text = $t['tweet_text']. ' '.$tags;
+	
 
 try {
-	//tweet tweet
-	$tweet = $twitter -> send(TWEET_TEXT, TMP_IMG);
-	
-	// remove the cam image 
-	unlink(TMP_IMG);
-	
-} catch (TwitterException $e) {
-	echo 'Error: ' . $e -> getMessage();
-}
+		//tweet tweet
+		$tweet = $twitter -> send($tweet_text, $tmp_img);
 
+		// remove the cam image
+		unlink($tmp_img);
+
+	} catch (TwitterException $e) {
+		echo 'Error: ' . $e -> getMessage();
+	}
+
+}
